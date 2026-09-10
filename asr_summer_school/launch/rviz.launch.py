58f50a237a068897623e4e61b75dca1f10338248
Madelon <madelon.bernardy@live.nl>
2026-09-10 12:45:52 +0200

    adds rviz to our package with now also per default the map

---
+++ asr_summer_school/launch/rviz.launch.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    rviz_config = os.path.join(
+        get_package_share_directory("asr_summer_school"), "config", "asr.rviz"
+    )
+
+    return LaunchDescription([
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            name="rviz2",
+            arguments=["-d", rviz_config],
+            output="screen",
+        ),
+    ])
