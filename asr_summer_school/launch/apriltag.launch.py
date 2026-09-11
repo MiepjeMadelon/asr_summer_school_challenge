2d48f3a4afdce565fca8099a46d353e0e7b0f17a
Tommaso Andina <tommaso@Tommasos-MacBook-Pro.local>
2026-09-11 09:23:36 +0200

    add file launch, and fix mission

--- asr_summer_school/launch/apriltag.launch.py
+++ asr_summer_school/launch/apriltag.launch.py
@@ -2,19 +2,22 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
-
-import os   
 
 
 def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
     return LaunchDescription([
+        DeclareLaunchArgument('use_sim_time', default_value='true'),
 
         Node(
             package='asr_summer_school',
             executable='apriltag_detector.py',
-            name='apriltag_detector',
-            parameters=[]
+            name='apriltag_subscriber',
+            output='screen',
+            parameters=[{
+                'use_sim_time': use_sim_time,
+                'max_range': 2.0,
+            }],
         ),
     ])
